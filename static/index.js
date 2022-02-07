80b527507b9efd09306924c0d511961a31fb41bf
naaru banna <nd20@gmail.com>
2022-02-07 14:24:46 +0530

    now site is ready

---
+++ static/index.js
@@ -0,0 +1,9 @@
+const authername = document.querySelector('authername')
+const autherinfo = document.querySelector('autherinfo')
+
+function hello() {
+    console.log('hello')
+    return
+}
+
+authername.addEventListener('click', hello)
